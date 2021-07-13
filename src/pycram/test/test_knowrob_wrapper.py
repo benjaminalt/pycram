f99547ef5a78f1d993178a61d4d7b62ab133a934
Benjamin Alt <benjamin_alt@outlook.com>
2021-07-13 16:19:41 +0200

    Updated KnowRob wrapper to new KnowRob APIs
    Update knowrob_wrapper.py due to breaking API changes in KnowRob:
    * kb_call/kb_project instead of ask/tell
    * load_owl instead of tripledb_load
    * Many tf_mongo_* functions were removed

--- src/pycram/test/test_knowrob_wrapper.py
+++ src/pycram/test/test_knowrob_wrapper.py
@@ -1,11 +1,27 @@
-import logging
+from unittest import TestCase
+
+from rospkg import RosPack
 
 from pycram.knowrob.knowrob_wrapper import KnowRob
 
-if __name__ == '__main__':
-    root = logging.getLogger()
-    root.setLevel(logging.INFO)
 
-    knowrob = KnowRob()
-    assert knowrob.load_owl("package://knowrob/owl/maps/iai_room_v1.owl", "map", "http://knowrob.org/kb/v1/IAI-Kitchen.owl#")
-    knowrob.once("tf_logger_enable")
+class TestKnowRobWrapper(TestCase):
+    def setUp(self) -> None:
+        self.rospack = RosPack()
+        self.knowrob = KnowRob(
+            clear_belief_state=True,
+            initial_belief_state=f"{self.rospack.get_path('knowrob_industrial')}/misc/mongo_knowrob_default/roslog"
+        )
+        self.knowrob.once(f"load_owl('{self.rospack.get_path('iai_semantic_maps')}/owl/kitchen.owl')")
+        self.knowrob.once(f"load_owl('{self.rospack.get_path('knowrob_industrial')}/owl/UR5Robotiq.owl')")
+        self.knowrob.once(f"urdf_load('http://knowrob.org/kb/UR5Robotiq.owl#UR5Arm_0',"
+                          f"'{self.rospack.get_path('pycram')}/resources/ur5_robotiq.urdf',"
+                          f"[load_rdf])")
+
+    def testKbCall(self):
+        res = self.knowrob.once(
+            "kb_call(new_iri(Episode, dul: 'Situation')),"
+            "kb_project(is_episode(Episode)),"
+            "kb_project(is_setting_for(Episode,'http://knowrob.org/kb/UR5Robotiq.owl#UR5Arm_0')),"
+            "kb_project(is_setting_for(Episode,'http://knowrob.org/kb/IAI-kitchen.owl#IAIKitchenMap_PM580j'))")
+        self.assertGreater(len(res), 0)
