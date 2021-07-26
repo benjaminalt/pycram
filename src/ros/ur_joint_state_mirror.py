5a2b2625138681b8c4bb18e8b1dccca354d7c1f4
Benjamin Alt <benjamin_alt@outlook.com>
2021-07-26 15:00:38 +0200

    URJointStateMirror: Allow global enable/disable
    Add a globally accessible flag to URJointStateMirror to allow clients to
    disable it. This is useful if one wants to switch between digital
    twinning and manual control of the simulated robot.

--- src/ros/ur_joint_state_mirror.py
+++ src/ros/ur_joint_state_mirror.py
@@ -5,6 +5,8 @@ import pybullet as pb
 
 from ros.rosbridge import ROSBridge
 
+DIGITAL_TWIN_ENABLED = True
+
 
 class URJointStateMirror(object):
     def __init__(self, world, joint_state_topic="/ur_digital_twin/joint_state"):
@@ -13,9 +15,10 @@ class URJointStateMirror(object):
         self.joint_state_sub = roslibpy.Topic(self.ros_client, joint_state_topic, "sensor_msgs/JointState")
 
     def control_simulated_ur(self, joint_state: roslibpy.Message):
-        logging.debug(f"URJointStateMirror::control_simulated_ur: {joint_state}")
-        joint_indices = list(range(1, 7))
-        pb.setJointMotorControlArray(self.world.robot.id, joint_indices, pb.POSITION_CONTROL, targetPositions=joint_state["position"])
+        if DIGITAL_TWIN_ENABLED:
+            logging.debug(f"URJointStateMirror::control_simulated_ur: {joint_state}")
+            joint_indices = list(range(1, 7))
+            pb.setJointMotorControlArray(self.world.robot.id, joint_indices, pb.POSITION_CONTROL, targetPositions=joint_state["position"])
 
     def __enter__(self):
         self.joint_state_sub.subscribe(self.control_simulated_ur)
