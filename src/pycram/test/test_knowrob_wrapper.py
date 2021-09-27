be31f880ac634288bdd64d0d81d7cbadcce90bcf
Benjamin Alt <benjamin_alt@outlook.com>
2021-09-27 15:55:58 +0200

    Small improvements for grounding designators via KnowRob

--- src/pycram/test/test_knowrob_wrapper.py
+++ src/pycram/test/test_knowrob_wrapper.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 
 from rospkg import RosPack
 
-from pycram.knowrob.knowrob_wrapper import KnowRob
+from pycram.knowrob import knowrob
 
 
 class TestKnowRobWrapper(TestCase):
@@ -12,7 +12,7 @@ class TestKnowRobWrapper(TestCase):
         logging.basicConfig(level=logging.INFO)
 
         self.rospack = RosPack()
-        self.knowrob = KnowRob(clear_beliefstate=True)  #,initial_belief_state=f"{self.rospack.get_path('knowrob_industrial')}/misc/mongo_knowrob_default/roslog"
+        knowrob.clear_beliefstate()
         self.kitchen_owl = os.path.join(self.rospack.get_path('iai_semantic_maps'), "owl", "kitchen.owl")
         self.kitchen_urdf = os.path.join(self.rospack.get_path('pycram'), "resources", "kitchen.urdf")
         self.kitchen_urdf_prefix = "http://knowrob.org/kb/IAI-kitchen.owl#"
@@ -23,13 +23,13 @@ class TestKnowRobWrapper(TestCase):
         self.robot_iri = "http://knowrob.org/kb/UR5Robotiq.owl#UR5Arm_0"
 
     def testKbCall(self):
-        self.knowrob.once(f"load_owl('{self.kitchen_owl}')")
-        self.knowrob.once(f"load_owl('{self.robot_owl}')")
-        self.knowrob.once(f"urdf_load('{self.robot_iri}',"
+        knowrob.once(f"load_owl('{self.kitchen_owl}')")
+        knowrob.once(f"load_owl('{self.robot_owl}')")
+        knowrob.once(f"urdf_load('{self.robot_iri}',"
                           f"'{self.robot_urdf}',"
                           f"[load_rdf])")
 
-        res = self.knowrob.once(
+        res = knowrob.once(
             "kb_call(new_iri(Episode, dul: 'Situation')),"
             "kb_project(is_episode(Episode)),"
             f"kb_project(is_setting_for(Episode,'{self.robot_iri}')),"
@@ -37,15 +37,15 @@ class TestKnowRobWrapper(TestCase):
         self.assertGreater(len(res), 0)
 
     def testCreateAction(self):
-        res = self.knowrob.neem_create_action()
+        res = knowrob.neem_create_action()
         self.assertTrue(res.startswith("http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#Action_"))
 
     def testStartEpisode(self):
-        act = self.knowrob.start_episode(self.kitchen_owl, self.kitchen_iri, self.kitchen_urdf,
+        act = knowrob.start_episode("soma:'PhysicalTask'", self.kitchen_owl, self.kitchen_iri, self.kitchen_urdf,
                                          self.kitchen_urdf_prefix, self.robot_owl, self.robot_iri, self.robot_urdf)
         self.assertIsNotNone(act)
 
     def testStopEpisode(self):
-        act = self.knowrob.start_episode(self.kitchen_owl, self.kitchen_iri, self.kitchen_urdf,
+        act = knowrob.start_episode(self.kitchen_owl, self.kitchen_iri, self.kitchen_urdf,
                                          self.kitchen_urdf_prefix, self.robot_owl, self.robot_iri, self.robot_urdf)
-        self.knowrob.stop_episode(os.path.join(self.rospack.get_path("pycram"), "neems", "test"))
+        knowrob.stop_episode(os.path.join(self.rospack.get_path("pycram"), "neems", "test"))
