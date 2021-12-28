39518022afec2b74621e0174dac1df47eec462cd
Benjamin Alt <benjamin_alt@outlook.com>
2021-12-28 22:59:05 +0100

    Reasoning: Grasp pose is just another feature frame

--- src/pycram/knowrob/reasoning.py
+++ src/pycram/knowrob/reasoning.py
@@ -41,19 +41,17 @@ def object_pose(object_iri: str, reference_cs: str = "world", timestamp=None) ->
 
 
 def grasp_pose(object_iri: str) -> List[float]:
-    query = f"""
-    kb_call(has_grasp_point({atom(object_iri)}, GraspPointName)),
-    mem_tf_get(GraspPointName, world, Pos, Ori)
-    """
-    res = knowrob.once(query)
-    pos = res["Pos"]
-    ori = res["Ori"]
-    return pos + ori
+    print(f"Getting grasp pose for {object_iri}")
+    grasp_point_name = knowrob.once(f"""
+        kb_call([
+            object_feature({atom(object_iri)}, Feature),
+            instance_of(Feature, ki:'GraspPoint'),
+            holds(Feature, soma:'hasNameString', FeatureName)
+        ])""")["FeatureName"]
+    return feature_pose(object_iri, grasp_point_name)
 
 
-def feature_pose(obj: Union[dict, ObjectDesignator], feature_name: str) -> List[float]:
-    object_grounded = obj.reference() if type(obj) == ObjectDesignator else obj
-    object_iri = object_grounded["name"]
+def feature_pose(object_iri, feature_name: str) -> List[float]:
     reference_frame, feature_frame_pos, feature_frame_ori = knowrob.once(f"""
         urdf_link_parent_joint({atom(object_iri)}, {atom(feature_name)}, FeatureFixedJoint),
         urdf_joint_origin({atom(object_iri)}, FeatureFixedJoint, FeatureFrameRelative)
@@ -62,9 +60,9 @@ def feature_pose(obj: Union[dict, ObjectDesignator], feature_name: str) -> List[
 
     res = knowrob.once(f"mem_tf_get({atom(object_iri)}, [world, Pos, Ori])")
     world_T_obj_pq = np.concatenate((res["Pos"], quaternion_wxyz_from_xyzw(res["Ori"])))
-    obj_T_world = transform_from_pq(world_T_obj_pq)
-    feature_T_obj = transform_from_pq(feature_frame_relative_pq)
-    feature_T_world = feature_T_obj @ obj_T_world
-    feature_frame_world_pq = pq_from_transform(feature_T_world)
+    world_T_obj = transform_from_pq(world_T_obj_pq)
+    obj_T_feature = transform_from_pq(feature_frame_relative_pq)
+    world_T_feature = world_T_obj @ obj_T_feature
+    feature_frame_world_pq = pq_from_transform(world_T_feature)
     feature_frame_world = np.concatenate((feature_frame_world_pq[:3], quaternion_xyzw_from_wxyz(feature_frame_world_pq[3:])))
     return feature_frame_world.tolist()
