bd8eb9853036277196736e8abed39a8385327a50
Benjamin Alt <benjamin_alt@outlook.com>
2022-01-11 16:38:11 +0100

    Object designator grounding via KnowRob: Object AABB
    Add grounding of axis-aligned-bounding boxes via KnowRob. This still uses the bullet world under the hood, though.

--- src/pycram/resolver/object_designator_grounding_knowrob.py
+++ src/pycram/resolver/object_designator_grounding_knowrob.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from pycram.bullet_world import BulletWorld
+from pycram.designator import DesignatorError
 from pycram.knowrob.reasoning import object_type, instances_of, object_pose
 from pycram.object_designator import LocatedObjectDesignatorDescription, ObjectDesignator
 
@@ -14,10 +15,22 @@ def _update_object_pose_in_bullet_world(object_name: str, object_pose: List):
         obj.set_position_and_orientation(object_pose[:3], object_pose[3:7])
 
 
-def ground_located_object(description: LocatedObjectDesignatorDescription):
-    def _ground_pose(desc):
-        desc.pose = object_pose(desc.name, desc.reference_frame, desc.timestamp)
+def _ground_aabb(desc: LocatedObjectDesignatorDescription):
+    if BulletWorld.current_bullet_world is None:
+        raise RuntimeError("Bullet world not initialized!!")
+    if len(BulletWorld.current_bullet_world.get_objects_by_name(desc.name)) == 0:
+        raise DesignatorError(f"No object with name {desc.name} in Bullet world")
+    if not desc.pose:
+        _ground_pose(desc)
+    _update_object_pose_in_bullet_world(desc.name, desc.pose)
+    desc.aabb = BulletWorld.current_bullet_world.get_objects_by_name(desc.name)[0].get_aabb()
 
+
+def _ground_pose(desc: LocatedObjectDesignatorDescription):
+    desc.pose = object_pose(desc.name, desc.reference_frame, desc.timestamp)
+
+
+def ground_located_object(description: LocatedObjectDesignatorDescription):
     if not description.type and not description.name:
         raise RuntimeError("Could not ground LocatedObjectDesignatorDescription: Either type or name must be given")
     if not description.type:
@@ -30,6 +43,8 @@ def ground_located_object(description: LocatedObjectDesignatorDescription):
             print(f"Locating object pose for {object_name}")
             description.name = object_name
             _ground_pose(description)
+            if description.aabb is None:
+                _ground_aabb(description)
             if BulletWorld.current_bullet_world is not None:
                 _update_object_pose_in_bullet_world(description.name, description.pose)
             yield description.__dict__
@@ -37,7 +52,10 @@ def ground_located_object(description: LocatedObjectDesignatorDescription):
     elif not description.pose:
         # Fetch the object pose and yield the grounded description
         _ground_pose(description)
-    _update_object_pose_in_bullet_world(description.name, description.pose)
+        if description.aabb is None:
+            _ground_aabb(description)
+    if BulletWorld.current_bullet_world is not None:
+        _update_object_pose_in_bullet_world(description.name, description.pose)
     yield description.__dict__
 
 
